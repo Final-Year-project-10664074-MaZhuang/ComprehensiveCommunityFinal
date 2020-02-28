5bce1fa294df65a4fe66e8cbbc14e05405e3f08f
SOFT356-Genshushu <zhuang.ma@students.plymouth.ac.uk>
2020-02-28 22:06:10 +0000

    add login, register,email activation account and logout

--- src/main/java/com/mz/finalcommunity/finalcommunity/service/UserService.java
+++ src/main/java/com/mz/finalcommunity/finalcommunity/service/UserService.java
@@ -1,15 +1,140 @@
 package com.mz.finalcommunity.finalcommunity.service;
 
+import com.mz.finalcommunity.finalcommunity.dao.LoginTicketMapper;
 import com.mz.finalcommunity.finalcommunity.dao.UserMapper;
+import com.mz.finalcommunity.finalcommunity.entity.LoginTicket;
 import com.mz.finalcommunity.finalcommunity.entity.User;
+import com.mz.finalcommunity.finalcommunity.util.CommunityConstant;
+import com.mz.finalcommunity.finalcommunity.util.CommunityUtil;
+import com.mz.finalcommunity.finalcommunity.util.MailClient;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 @Service
-public class UserService {
+public class UserService implements CommunityConstant {
+    @Autowired
+    private LoginTicketMapper loginTicketMapper;
     @Autowired
     private UserMapper userMapper;
+    @Autowired
+    private MailClient mailClient;
+    @Autowired
+    private TemplateEngine templateEngine;
+    @Value("${community.path.domain}")
+    private String domain;
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
     public User findUserById(int id){
         return userMapper.selectById(id);
+    }
+
+    public Map<String, Object> register(User user) {
+        Map<String, Object> map = new HashMap<>();
+
+        if(user==null){
+            throw new IllegalArgumentException("Not Null");
+        }
+        if(StringUtils.isBlank(user.getUsername())){
+            map.put("usernameMsg","username is null");
+            return map;
+        }
+        if(StringUtils.isBlank(user.getPassword())){
+            map.put("passwordMsg","password is null");
+            return map;
+        }
+        if(StringUtils.isBlank(user.getEmail())){
+            map.put("emailMsg","Email is null");
+            return map;
+        }
+
+        User u = userMapper.selectByName(user.getUsername());
+        if(u!=null){
+            map.put("usernameMsg","Username is exist");
+            return map;
+        }
+        u = userMapper.selectByEmail(user.getEmail());
+        if(u!=null){
+            map.put("emailMsg","Email is exist");
+            return map;
+        }
+        //register
+        user.setSalt(CommunityUtil.generateUUID().substring(0,5));
+        user.setPassword(CommunityUtil.md5(user.getPassword()+user.getSalt()));
+        user.setType(0);
+        user.setStatus(0);
+        user.setActivationCode(CommunityUtil.generateUUID());
+        user.setHeaderUrl(String.format("http://images.nowcoder.com/head/%dt.png",new Random().nextInt(1000)));
+        user.setCreateTime(new Date());
+        userMapper.insertUser(user);
+        //send email
+        Context context = new Context();
+        context.setVariable("email",user.getEmail());
+        String url = domain+contextPath+"/activation/"+user.getId()+"/"+user.getActivationCode();
+        context.setVariable("url",url);
+        String content = templateEngine.process("/mail/activation",context);
+        mailClient.sendMail(user.getEmail(),"Activate your account",content);
+        return map;
+    }
+
+    public int activation(int userId, String code) {
+        User user = userMapper.selectById(userId);
+        if(user.getStatus()==1){
+            return ACTIVATION_REPEAT;
+        }else if(user.getActivationCode().equals(code)){
+            userMapper.updateStatus(userId,1);
+            return ACTIVATION_SUCCESS;
+        }else {
+            return ACTIVATION_FAILURE;
+        }
+    }
+
+    public Map<String, Object> login(String username, String password, int expiredSeconds) {
+        Map<String, Object> map = new HashMap<>();
+
+        if(StringUtils.isBlank(username)){
+            map.put("usernameMsg","uasername can not be null");
+            return map;
+        }
+        if(StringUtils.isBlank(password)){
+            map.put("passwordMsg","password can not be null");
+            return map;
+        }
+        User user = userMapper.selectByName(username);
+        if(user==null){
+            map.put("usernameMsg","uasername can not be exist");
+            return map;
+        }
+        if(user.getStatus()==0){
+            map.put("usernameMsg","uasername can not be Activate");
+            return map;
+        }
+        password=CommunityUtil.md5(password+user.getSalt());
+        if(!user.getPassword().equals(password)){
+            map.put("passwordMsg","password Error");
+            return map;
+        }
+
+        LoginTicket loginTicket = new LoginTicket();
+        loginTicket.setUserId(user.getId());
+        loginTicket.setTicket(CommunityUtil.generateUUID());
+        loginTicket.setStatus(0);
+        loginTicket.setExpired(new Date(System.currentTimeMillis()+expiredSeconds * 1000));
+        loginTicketMapper.insertLoginTicket(loginTicket);
+
+        map.put("ticket",loginTicket.getTicket());
+        return map;
+    }
+
+    public void logout(String ticket) {
+        loginTicketMapper.updateStatus(ticket,1);
     }
 }
