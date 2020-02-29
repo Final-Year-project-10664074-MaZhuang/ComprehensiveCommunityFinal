3974a0108a77ba63e29217811e63445056930ddd
SOFT356-Genshushu <zhuang.ma@students.plymouth.ac.uk>
2020-02-29 08:12:24 +0000

    Add interceptor

--- src/main/java/com/mz/finalcommunity/finalcommunity/config/WebMvcConfig.java
+++ src/main/java/com/mz/finalcommunity/finalcommunity/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.mz.finalcommunity.finalcommunity.config;
 
+import com.mz.finalcommunity.finalcommunity.controller.interceptor.LoginRequiredInterceptor;
 import com.mz.finalcommunity.finalcommunity.controller.interceptor.LoginTicketInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -10,10 +11,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
     @Autowired
     private LoginTicketInterceptor loginTicketInterceptor;
-
+@Autowired
+private LoginRequiredInterceptor loginRequiredInterceptor;
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginTicketInterceptor)
+                .excludePathPatterns("/**/*.css","/**/*.js","/**/*.png","/**/*.jpg","/**/*.jpeg");
+        registry.addInterceptor(loginRequiredInterceptor)
                 .excludePathPatterns("/**/*.css","/**/*.js","/**/*.png","/**/*.jpg","/**/*.jpeg");
     }
 }
