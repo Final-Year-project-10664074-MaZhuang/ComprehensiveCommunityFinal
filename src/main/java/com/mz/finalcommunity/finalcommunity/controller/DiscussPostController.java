fe0e8e0a874751e04ffb80635865178232a55684
SOFT356-Genshushu <zhuang.ma@students.plymouth.ac.uk>
2020-02-29 19:55:23 +0000

    add article detail page

--- src/main/java/com/mz/finalcommunity/finalcommunity/controller/DiscussPostController.java
+++ src/main/java/com/mz/finalcommunity/finalcommunity/controller/DiscussPostController.java
@@ -3,10 +3,13 @@ package com.mz.finalcommunity.finalcommunity.controller;
 import com.mz.finalcommunity.finalcommunity.entity.DiscussPost;
 import com.mz.finalcommunity.finalcommunity.entity.User;
 import com.mz.finalcommunity.finalcommunity.service.DiscussPostService;
+import com.mz.finalcommunity.finalcommunity.service.UserService;
 import com.mz.finalcommunity.finalcommunity.util.CommunityUtil;
 import com.mz.finalcommunity.finalcommunity.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,6 +24,9 @@ public class DiscussPostController {
 
     @Autowired
     private HostHolder hostHolder;
+
+    @Autowired
+    private UserService userService;
 
     @RequestMapping(path = "/add",method = RequestMethod.POST)
     @ResponseBody
@@ -38,5 +44,15 @@ public class DiscussPostController {
 
         //todo error
         return CommunityUtil.getJSONString(0,"Published successfully!!");
+    }
+
+    @RequestMapping(path = "/detail/{discussPostId}",method = RequestMethod.GET)
+    public String getDiscussPost(@PathVariable("discussPostId") int discussPostId, Model model){
+        DiscussPost post = discussPostService.findDiscussPostById(discussPostId);
+        model.addAttribute("post",post);
+        User user = userService.findUserById(post.getUserId());
+        model.addAttribute("user",user);
+        // TODO: 29/02/2020
+        return "/site/discuss-detail";
     }
 }
