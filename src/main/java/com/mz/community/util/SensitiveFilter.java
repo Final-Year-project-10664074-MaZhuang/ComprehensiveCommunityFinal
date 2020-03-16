44f0717bbe622d1ea6b7294ae31af0777853b7af
SOFT356-Genshushu <zhuang.ma@students.plymouth.ac.uk>
2020-03-16 05:57:42 +0000

    add Sensitive word filtering

--- src/main/java/com/mz/community/util/SensitiveFilter.java
+++ src/main/java/com/mz/community/util/SensitiveFilter.java
@@ -1,5 +1,7 @@
 package com.mz.community.util;
 
+import org.apache.commons.lang3.CharUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -55,6 +57,67 @@ public class SensitiveFilter {
                 tempNode.setKeyWordEnd(true);
             }
         }
+    }
+
+    /**
+     * Filter sensitive words
+     *
+     * @param text Text to be filtered
+     * @return Filtered text
+     */
+    public String filter(String text) {
+        if (StringUtils.isBlank(text)) {
+            return null;
+        }
+        //pointer1 Pointing to tree
+        TrieNode tempNode = rootNode;
+        //pointer2 pointing to the begin of the word
+        int begin = 0;
+        //pointer2 pointing to the current position of the word
+        int position = 0;
+        //Filter results
+        StringBuilder sb = new StringBuilder();
+        while (position < text.length()) {
+            char c = text.charAt(position);
+            //Skip symbol
+            if (isSymbol(c)) {
+                //if pointer1 pointing to root node,Add this symbol to the result,pointer2 move to next char
+                if (tempNode == rootNode) {
+                    sb.append(c);
+                    begin++;
+                }
+                position++;
+                continue;
+            }
+            //check next node
+            tempNode = tempNode.getSubNode(c);
+            if (tempNode == null) {
+                //Strings starting with pointer 1 are not sensitive words
+                sb.append(text.charAt(begin));
+                //move to next char
+                position = ++begin;
+                //pointer 1 pointing root node
+                tempNode = rootNode;
+            } else if (tempNode.isKeyWordEnd()) {
+                //find sensitive,replace begin~position
+                sb.append(REPLACEMENT);
+                //move to next char
+                begin = ++position;
+                //pointer 1 pointing root node
+                tempNode = rootNode;
+            } else {
+                //move to next char
+                position++;
+            }
+        }
+        //Add the last characters to the result
+        sb.append(text.substring(begin));
+        return sb.toString();
+    }
+
+    //Determine if it is a symbol
+    private boolean isSymbol(Character c) {
+        return !CharUtils.isAsciiAlphanumeric(c) && (c < 0x2E80 || c > 0x9FFF);
     }
 
     //Prefix tree
