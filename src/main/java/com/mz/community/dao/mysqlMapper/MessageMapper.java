6aab2a941fb9dea9dae18f025bd61c23a08e94a3
SOFT356-Genshushu <zhuang.ma@students.plymouth.ac.uk>
2020-03-21 01:35:32 +0000

    add message function

---
+++ src/main/java/com/mz/community/dao/mysqlMapper/MessageMapper.java
@@ -0,0 +1,43 @@
+package com.mz.community.dao.mysqlMapper;
+
+import com.mz.community.entity.Message;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface MessageMapper {
+    //Query the current user's session list,Return only the latest piece of data per session
+    List<Message> selectConversations(int userId, int offset, int limit);
+
+    //Query the number of sessions for the current user
+    int selectConversationCount(int userId);
+
+    //Query the list of private messages contained in a conversation
+    List<Message> selectLetters(String conversationId, int offset, int limit);
+
+    //Query the number of sessions
+    int selectLetterCount(String conversationId);
+
+    //Query the number of unread messages
+    int selectLetterUnread(int userId, String conversationId);
+
+    //send message
+    int insertMessage(Message message);
+
+    //update message status
+    int updateStatus(List<Integer> ids, int status);
+
+    //Query the latest notifications under a certain topic
+    Message selectLatestNotice(int userId, String topic);
+
+    //Query the number of latest notifications on a topic
+    int selectNoticeCount(int userId, String topic);
+
+    //Query the number of unread notifications
+    int selectNoticeUnreadCount(int userId, String topic);
+
+    //Query the notification list of a topic
+    List<Message> selectNotices(int userId, String topic, int offset, int limit);
+
+}
